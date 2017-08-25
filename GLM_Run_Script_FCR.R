7cf190f04fa0fadadc8d2035061884d136009b12
melofton <melofton@vt.edu>
2017-08-25 13:19:03 -0400

    starting to add diagnostics to this

--- GLM_Run_Script_FCR.R
+++ GLM_Run_Script_FCR.R
@@ -3,11 +3,20 @@
 # Load packages, set sim folder, load nml file ####
 library(GLMr)
 library(glmtools)
+glm_version() #check to make sure you are running 2.1.8!
 
 sim_folder <- getwd() ##!! Edit this line of code (maybe-- if wd is not via GitHub repo) to redefine your sim_folder
 
+#Create folders for today's sims in working directory - help w/ automating of setting up and saving sims!??
+for (i in 1:5){
+  filename = paste0("Sim_",i)
+  dir.create(filename)
+  
+}
+
 nml_file <- paste0(sim_folder,"/glm2.nml") 
 nml <- read_nml(nml_file) 
+print(nml)
 
 # Run GLM ####
 run_glm(sim_folder, verbose=TRUE)
@@ -20,12 +29,48 @@ var_names <- sim_vars(nc_file)
 field_stage = file.path(sim_folder, 'FCR_stage_observed_022317.csv')
 plot_compare_stage(nc_file,field_stage, main="dots = observed") # observed vs. sim lake stage
 
-field_file <- file.path(sim_folder, 'FCR_2015_CTD_wtr.csv') # Define the observed field data
-plot_temp_compare(nc_file, field_file) # Plot your GLM simulated data vs. the observed data 
+field_file <- file.path(sim_folder, 'FCR_2015_CTD_temp.csv') # Define the observed field data for temp
+DO_field <- file.path(sim_folder, 'FCR2015_CTD_DO.csv') #ditto for DO
+plot_temp_compare(nc_file, field_file) # Plot your GLM simulated data vs. the observed data
+
+#need to run lines 71-94 before playing with DO
+plot_var_compare(nc_file,DO_field,var_name = "DO")
+
+#Run sim diagnostics
+
+#see what vars are available for diagnostics
+sim_metrics(with_nml = TRUE)
+
+#run RMSE using function
+myTempResampled = resample_to_field(nc_file,field_file, method = 'interp')
+myDOResampled = resample_to_field(nc_file,DO_field,var_name = "DO", method = 'interp')
+
+RMSE = function(m, o){
+  sqrt(mean((m - o)^2))
+}
+m_temp <- myTempResampled$Modeled_temp[32:1488]
+o_temp <- myTempResampled$Observed_temp[32:1488]
+
+RMSE(m_temp,o_temp)
+
+m_DO <- myDOResampled$Modeled_DO[1:1457]
+o_DO <- myDOResampled$Observed_DO[1:1457]
+
+RMSE(m_DO,o_DO)
+
+#run RMSE using glmtools
+#temp
+compare_to_field(nc_file, field_file, nml_file = nml_file, metric = 'thermo.depth', as_value = FALSE,
+                 na.rm = TRUE, precision = 'days',method = 'interp')
+
 
 # OPTIONAL PART 2: Nicole's unit conversion script ####
 ## Note that not all Sunapee output vars are included in current FCR runs ##
 ## Those variables are currently hashed out ##
+
+#Load packages
+library(reshape2)
+library(ggplot2)
 
 # Switches
 ConvertVariables = TRUE
@@ -58,11 +103,11 @@ plot_temp(SimFile)
 
 # DO
 plot_var(SimFile,'DO')
-DO <- get_var(SimFile,'DO', z_out = c(1, 5, 10, 20), reference = 'surface') # pull DO from 1m below surface
+DO <- get_var(SimFile,'DO', z_out = c(1, 3, 5, 9), reference = 'surface') # pull DO from 1m below surface
 DO_long <- melt(DO, id.vars=c("DateTime"))
 
 ggplot(DO_long, aes(y=value, x=DateTime, colour=variable)) + 
-  geom_line(lwd=1.05) + mytheme +
+  geom_line(lwd=1.05) +
   scale_y_continuous("Dissolved Oxygen (mg/L)")
 
 # Nutrients
@@ -72,7 +117,7 @@ plot_var(SimFile,var_name = 'DOC')
 #plot_var(SimFile,var_name = 'TotN2')
 plot_var(SimFile,var_name = 'NitN')
 plot_var(SimFile,var_name = 'NIT_denit',col_lim=c(0,0.0000001))
-
+plot_var(SimFile,var_name = 'CAR_pH')
 plot_var(SimFile,var_name = 'PHY_NUP')
 plot_var(SimFile,var_name = 'PHY_TPHYS')
 plot_var(SimFile,var_name = 'PHY_TCHLA')
@@ -110,4 +155,4 @@ plot(temp,type='l')
 chla <- get_var(SimFile,var_name = 'PHY_TCHLA',z_out = 1,reference = 'surface')
 write.csv(chla,"./model_output/chla_output_surface_2015.csv",row.names = FALSE,quote = FALSE)
 
-lec <- get_var(SimFile,var_name = 'extc_coef',z_out=1,reference = 'surface')
+lec <- get_var(SimFile,var_name = 'extc_coef',z_out=1,reference = 'surface') ##what is this??
